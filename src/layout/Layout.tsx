42ca303b61ae4cbf22dff8544ecb2cce6979bb9c
Daler <dsarifkulo021@gmail.com>
2023-04-05 03:33:48 +0500

    aside bar is also ready

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -67,16 +67,21 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
 							<IoMdNotificationsOutline size={24} />
 						</Link>
 						<div className="lang-div px-3 h-full">
-							<Dropdown  arr={['ru', 'uz', 'ko']}>
+							<Dropdown arr={["ru", "uz", "ko"]}>
 								<button className="flex items-center gap-2">
-									<span className="opacity-[.5]" >
-                                        RU
-                                    </span>
+									<span className="opacity-[.5]">RU</span>
 									<BiChevronDown size={20} color="#909090" />
 								</button>
 							</Dropdown>
 						</div>
-						<Dropdown arr={['Dashboard', 'Settings', 'Earnings', 'Sign out']} >
+						<Dropdown
+							arr={[
+								"Dashboard",
+								"Settings",
+								"Earnings",
+								"Sign out",
+							]}
+						>
 							<button className="flex items-center gap-2">
 								<Image
 									src="/icons/logo_only.svg"
@@ -90,62 +95,102 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
 						</Dropdown>
 					</nav>
 				</header>
-				{menuOpen && (
-					<aside className="w-[30%] h-[100vh] bg-[#232323]">
-						<div className="flex justify-between items-center py-6 px-7 bg-[#1F1E1E]">
+
+				<aside
+					className="h-[100vh] bg-[#232323]"
+					style={{ width: menuOpen ? "30%" : "5%" }}
+				>
+					<div className="flex justify-between items-center py-6 px-7 bg-[#1F1E1E]">
+						{menuOpen && (
 							<Image
 								src="/icons/big_logo.svg"
 								alt=""
 								width="100"
 								height="100"
 							/>
-							<button>
-								<AiOutlineMenu size="24" color="white" />
-							</button>
+						)}
+						<button onClick={() => setMenuOpen(!menuOpen)}>
+							<AiOutlineMenu size="24" color="white" />
+						</button>
+					</div>
+					<div className="flex flex-col gap-8 py-6 px-7">
+						<div>
+							{!menuOpen && <FiHome size={24} color="white" />}
+							<h3 className="flex gap-3 items-center text-white text-xl font-medium">
+								{menuOpen && <span>Меню</span>}
+							</h3>
+							{menuOpen && (
+								<>
+									<nav className="flex flex-col gap-4 px-9 py-6">
+										<Link
+											href="#"
+											className="text-base text-white"
+										>
+											Пациенты
+										</Link>
+										<Link
+											href="#"
+											className="text-base text-white"
+										>
+											Клиники
+										</Link>
+										<Link
+											href="#"
+											className="text-base text-white"
+										>
+											Источники
+										</Link>
+										<Link
+											href="#"
+											className="text-base text-white"
+										>
+											Координаторы
+										</Link>
+									</nav>
+									<hr className="opacity-[.3]" />
+								</>
+							)}
 						</div>
-						<div className="flex flex-col gap-8 py-6 px-7">
-							<div>
-								<h3 className="flex gap-3 items-center text-white text-xl font-medium">
-									<FiHome size={24} color="white" />
-									Меню
-								</h3>
-								<nav className="flex flex-col gap-4 px-9 py-6">
-									<Link
-										href="#"
-										className="text-base text-white"
-									>
-										Пациенты
-									</Link>
-									<Link
-										href="#"
-										className="text-base text-white"
-									>
-										Клиники
-									</Link>
-									<Link
-										href="#"
-										className="text-base text-white"
-									>
-										Источники
-									</Link>
-									<Link
-										href="#"
-										className="text-base text-white"
-									>
-										Координаторы
-									</Link>
-								</nav>
-								<hr className="opacity-[.3]" />
-							</div>
+						{menuOpen && (
 							<div>
 								<h3 className="flex gap-3 items-center text-white text-xl font-medium">
 									<FiFilter size={24} color="white" />
 									Фильтр
 								</h3>
+								<div className="custom-scroll flex flex-col gap-4 mt-8 h-[400px] overflow-y-scroll">
+									<span className="text-base text-white font-medium">
+										Статус
+									</span>
+									{[1, 2, 3, 4, 5].map((item) => (
+										<div className="flex items-center gap-2.5">
+											<input
+												type="checkbox"
+												className="bg-[#484848] rounded-sm w-4 h-4"
+											/>
+											<span className="text-white">
+												Новое
+											</span>
+										</div>
+									))}
+									<span className="text-base text-white font-medium mt-6">
+										Координатор
+									</span>
+									{[1, 2, 3, 4, 5].map((item) => (
+										<div className="flex items-center gap-2.5">
+											<input
+												type="checkbox"
+												className="bg-[#484848] rounded-sm w-4 h-4"
+											/>
+											<span className="text-white">
+												Новое
+											</span>
+										</div>
+									))}
+								</div>
 							</div>
-						</div>
-					</aside>
-				)}
+						)}
+					</div>
+				</aside>
 			</div>
 			<main className="pl-[25%] pt-[100px]">{children}</main>
 			<footer></footer>
